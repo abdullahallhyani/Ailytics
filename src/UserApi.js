60036e381fe1fc813bd30fad7c82d68196641391
ghosoonfk <ghosooonf@gmail.com>
2021-09-24 10:06:05 +0300

    demoInfo

--- src/UserApi.js
+++ src/UserApi.js
@@ -1,10 +1,17 @@
 {
   users[
-    {
-      userEmail: "test@test.com",
-      token: "123abcd",
+    ({
+      userEmail: "admin@test.com",
+
       password: 12345,
-    }
+      role: "admin",
+    },
+    {
+      userEmail: "user@test.com",
+
+      password: 12345,
+      role: "admin",
+    })
   ];
 }
 export default users;
